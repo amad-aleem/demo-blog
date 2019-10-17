cd551d27aa59e59ac2e5d328bf276435b6b31d40
Usman Asif <usman.a@goboon.co>
2019-10-17 18:04:05 +0500

    Devise implemented and users controlled added for RUD

--- Gemfile
+++ Gemfile
@@ -5,7 +5,11 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem 'bootstrap', '~> 4.3.1'
 
+gem 'jquery-rails'
+
+gem 'devise', '~> 4.7', '>= 4.7.1'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.7'
 # Use postgresql as the database for Active Record
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, except: %i[index]
+  def index
+    @users = User.all
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = 'Edited successfully'
+      redirect_to @user
+    else
+      flash[:alert] = 'Unable to edit'
+      render 'edit'
+    end
+  end
+
+  def destroy
+    flag = check_self
+    return redirect_to users_path if flag
+
+    if @user.destroy
+      flash[:notice] = 'Deleted successfully'
+      redirect_to users_path
+      return
+    else
+      flash[:alert] = 'Unable to delete'
+    end
+    redirect_to users_path
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email)
+  end
+
+  def check_self
+    if current_user == @user
+      flash[:alert] = 'You cannot delete yourself!'
+      true
+    end
+  end
+end
